54b00ba2a73a9d23b58056d7f585cb23a2501dbb
Sravani <sravanilal1986@gmail.com>
2023-08-16 12:00:29 +0530

    committing 2nd practice

---
+++ src/Scannerdemo1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Scannerdemo1 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Please enter name");
+        String name= input.next();
+        System.out.println("Please enter age");
+        int age= input.nextInt();
+        boolean isMajor;
+        if (age > 18) {
+            System.out.println(name + "is a Major");
+        }
+        else {
+            System.out.println(name + "is a Minor");
+        }
+    }
+}
